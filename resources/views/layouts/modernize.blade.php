7acdedb4d4815ef5391918db7421605064627e91
Thura Sitt Naing <coding2am@gmail.com>
2024-06-14 16:44:50 +0630

    Fix Dashboard Menu

--- resources/views/layouts/modernize.blade.php
+++ resources/views/layouts/modernize.blade.php
@@ -32,22 +32,7 @@
                     </div>
                 </div>
                 <!-- Sidebar navigation-->
-                <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
-                    <ul id="sidebarnav">
-                        <li class="nav-small-cap">
-                            <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-                            <span class="hide-menu text-primary">Main Menus</span>
-                        </li>
-                        <li class="sidebar-item">
-                            <a class="sidebar-link" href="" aria-expanded="false">
-                                <span>
-                                    <i class="ti ti-layout-dashboard"></i>
-                                </span>
-                                <span class="hide-menu">Dashboard</span>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
+                @include('layouts.menu')
                 <!-- End Sidebar navigation -->
             </div>
             <!-- End Sidebar scroll-->
