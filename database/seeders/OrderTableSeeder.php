9bf85129a30456a40333c7dc378cdb795c2cbcdc
YadanarShoonHtet <yadanarshyunhtet.1022@gmail.com>
2024-06-20 22:03:55 +0630

    ordertableseeder

---
+++ database/seeders/OrderTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\OrderTables;
+
+class OrderTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $order_tables =[
+        //     [
+        //         'table_no' =>generateTableNo(),
+        //         'max_person' =>'21',
+        //         'active'=>'1'
+        //     ]
+        // ];
+
+        for ($i = 0; $i < 30; $i++) {
+            $order_tables =[
+            [
+                'table_no' =>generateTableNo(),
+                'max_person' =>'21',
+                'active'=>'1'
+            ]
+        ];
+            foreach ($order_tables as $order_table) {
+                OrderTables::create($order_table);
+            }
+        }
+
+
+    }
+}
