964971b09624fe9c373395a734a2ddff00de957f
YadanarShoonHtet <yadanarshyunhtet.1022@gmail.com>
2024-06-24 11:04:37 +0630

    add ingredient crud

--- app/Http/Controllers/IngredientController.php
+++ app/Http/Controllers/IngredientController.php
@@ -2,9 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\IngredientStoreRequest;
+
+use App\Models\Ingredient;
+use App\Services\IngredientService;
 use Illuminate\Http\Request;
 
 class IngredientController extends Controller
 {
-    //
+    private $ingredientService;
+
+    public function __construct(IngredientService $ingredientService)
+    {
+        $this->ingredientService = $ingredientService;
+    }
+    public function index()
+    {
+        $data = $this->ingredientService->index();
+       return view('backend/ingredient.index',$data);
+    }
+    public function create()
+    {
+        return view('backend/ingredient.create');
+    }
+
+    public function store( IngredientStoreRequest $request)
+    {
+        Ingredient::create([
+            'name' => $request->name,
+        ]);
+
+        return redirect()->route('ingredient.index');
+    }
+    public function destroy(Ingredient $ingredient)
+    {
+        $data =$this->ingredientService->destroy($ingredient);
+        return redirect()->route('ingredient.index')->with($data['status'],$data['message']);
+    }
+
+    public function edit(Ingredient $ingredient)
+    {
+        return view('backend/ingredient.edit', compact('ingredient'));
+    }
+
+    public function update(Request $request,Ingredient $ingredient)
+    {
+       $data =$this->ingredientService->update($request,$ingredient);
+        return redirect()->route('ingredient.index')->with($data['status'],$data['message']);
+    }
+
+
 }
--- resources/views/backend/ingredient/index.blade.php
+++ resources/views/backend/ingredient/index.blade.php
@@ -1,1 +1,109 @@
 
+@extends('layouts.modernize')
+
+@section('title', 'Main Dashboard')
+
+@section('page', 'Ingredient')
+
+@section('content')
+    <div class="container-fluid">
+        @php
+            $name = $_GET['name'] ?? '';
+        @endphp
+        <div class="card">
+            <div class="card-header">Filter</div>
+            <form action="" method="GET" autocomplete="off">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="mb-3 col-md-3">
+                            <input type="text" name="name" class="form-control" placeholder="Name" value="{{ $name }}">
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer d-flex justify-content-end">
+                    <div>
+                        <a href="{{ route('ingredient.index') }}" class="btn btn-danger">Clear</a>
+                        <button type="submit" class="btn btn-primary">
+                            <i class="ti ti-search me-1"></i>Search
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="card">
+            <div class="card-header py-3 px-4">
+                <div class="d-flex justify-content-between align-items-center">
+                    <div>
+                        <h3 class="card-title">Ingredients</h3>
+                        <p class="text-primary">Total Ingredients:{{ $count }}</p>
+                    </div>
+                    <div>
+                        <div class="d-flex justify-content-end">
+                            <a href="{{ route('ingredient.create') }}" class="btn btn-primary"><i class="ti ti-plus"></i>
+                                Add
+                                New Ingredient</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body p-0"></div>
+                <div class="table-responsive">
+                    <table class="table table-bordered text-center mb-0">
+                        <thead class="">
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Description</th>
+                                <th scope="col">Created at</th>
+                                <th scope="col">
+                                    <center>Action</center>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse ($ingredients as $key=> $ingredient)
+                            <tr>
+                                <td>{{ ++$i }}.</td>
+                                <td>{{ $ingredient->name }}</td>
+                                <td>{{ $ingredient->description }}</td>
+                                <td>{{ $ingredient->created_at->format('d/m/Y') }}</td>
+                                <td align="center">
+                                    <div class="d-flex justify-content-center">
+
+                                        <a href="{{ route('ingredient.edit', ['ingredient'=> $ingredient]) }}"
+                                            class="btn btn-sm btn-warning me-1">Edit</a>
+                                        <form action="{{ route('ingredient.destroy', ['ingredient'=> $ingredient]) }}"
+                                            method="POST">
+                                            @csrf
+                                            @method('delete')
+
+                                            <button type="submit"
+                                                onClick="return confirm('Are you sure?')"class="btn btn-sm btn-danger">Delete</button>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+
+                            @empty
+                            <tr>
+                                <td align="center" colspan="6">There is no ingredient yet!</td>
+                            </tr>
+                            @endforelse
+                        </tbody>
+
+                    </table>
+
+            </div>
+        </div>
+
+        </div>
+    </div>
+@endsection
+
+@section('css')
+
+@endsection
+
+@section('js')
+
+@endsection
