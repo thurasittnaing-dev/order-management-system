36039dcae5bfb88be23f33d9235afb1f13c4c0e1
TheintShweYiHtike <htiketheint1@gmail.com>
2024-06-28 10:45:33 +0630

    change reciepes.blade.php

--- resources/views/order/recipes.blade.php
+++ resources/views/order/recipes.blade.php
@@ -13,31 +13,35 @@
                 @foreach ($categories as $category)
                     <div class="col-md-2">
                         <li class="nav-item d-grid" role="presentation">
-                            <button class="nav-link" id="{{ $category->name }}-tab" data-bs-toggle="tab"
-                                data-bs-target="#{{ $category->name }}food" type="button" role="tab"
-                                aria-controls="{{ $category->name }}food" aria-selected="false">
+                            <button class="nav-link" id="{{ $category->id}}-tab" data-bs-toggle="tab"
+                                data-bs-target="#{{ $category->id}}" type="button" role="tab"
+                                aria-controls="{{ $category->id}}" aria-selected="false">
                                 {{ $category->name }}</button>
                         </li>
                     </div>
                 @endforeach
             </ul>
+
             <div class="tab-content" id="myTabContent">
-                <div class="tab-pane fade" id="chinesefood" role="tabpanel"
-                    aria-labelledby="chinesefood-tab">
-                    <div class="mt-3">
-                        <div class="row g-2">
-                            @for ($i=0 ; $i<10 ; $i++)
-                                <a href="" class="col-md-2">
-                                    <div class="table-card">
-                                        <img src="{{ asset('images/mala.png') }}" class="table-img" alt="">
-                                        <span class="table-badge">Mala Xiang Gou</span>
-                                    </div>
-                                </a>
-                            @endfor
+                @foreach ($categories as $category)
+                    <div class="tab-pane fade" id="{{ $category->id}}" role="tabpanel"
+                        aria-labelledby="{{ $category->id}}-tab">
+                        <div class="mt-3">
+                            <div class="row g-2">
+                                @for ($i=0 ; $i<10 ; $i++)
+                                    <a href="" class="col-md-2">
+                                        <div class="table-card">
+                                            <img src="{{ asset('images/mala.png') }}" class="table-img" alt="">
+                                            <span class="table-badge">Mala Xiang Gou</span>
+                                        </div>
+                                    </a>
+
+                                @endfor
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="tab-pane fade" id="koreanfood" role="tabpanel"
+                @endforeach
+                {{-- <div class="tab-pane fade" id="koreanfood" role="tabpanel"
                     aria-labelledby="koreanfood-tab">
                     <div class="mt-3">
                         <div class="row g-2">
@@ -66,7 +70,7 @@
                             @endfor
                         </div>
                     </div>
-                </div>
+                </div> --}}
             </div>
         </div>
     </div>
