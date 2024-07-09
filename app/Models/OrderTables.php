9960d528a6afea5b5e7960fe8a1ec3bca525b147
Thura Sitt Naing <coding2am@gmail.com>
2024-07-09 11:53:01 +0630

    Update Fix

--- app/Models/OrderTables.php
+++ app/Models/OrderTables.php
@@ -5,14 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Room;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class OrderTables extends Model
 {
     use HasFactory;
-    protected $fillable  = ['table_no', 'max_person', 'active','in_used','room_id'];
+    protected $fillable  = ['table_no', 'max_person', 'active', 'in_used', 'room_id'];
 
     public function room()
     {
         return $this->belongsTo(Room::class);
+    }
+
+    public function current_order(): HasOne
+    {
+        return $this->hasOne(Order::class, 'order_table_id', 'id')->where('status', false);
     }
 }
