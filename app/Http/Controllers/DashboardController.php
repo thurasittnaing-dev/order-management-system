f6f9260c4efa2fdadf62971ce2a84d6c8d58b4dc
WahWahAung695 <wahwahaung695@gmail.com>
2024-07-20 20:47:25 +0630

    update monthly invoices chart

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -24,37 +24,6 @@ class DashboardController extends Controller
         $monthsOfYear = $this->getMonthlyIncomeData($selectedYear);
         $monthsOfYearInvoices = $this->getMonthlyInvoicesData($selectedInvoiceYear);
 
-        // // Fetch monthly income for the current year
-        // $monthlyIncome = Order::select(
-        //     DB::raw('MONTH(created_at) as month'),
-        //     DB::raw('SUM(net_amount) as total_income')
-        // )->whereYear('created_at', $selectedYear)
-        // ->groupBy('month')
-        // ->orderBy('month')
-        // ->get();
-
-        // // Fetch monthly total invoices for the current year
-        // $monthlyInvoices = Order::select(
-        //     DB::raw('MONTH(created_at) as month'),
-        //     DB::raw('COUNT(id) as total_invoices')
-        // )->whereYear('created_at', $selectedYear)
-        // ->groupBy('month')
-        // ->orderBy('month')
-        // ->get();
-
-        // // Initialize an array with all months set to 0
-        // $monthsOfYear = array_fill(1, 12, 0);
-        // $monthsOfYearInvoices = array_fill(1, 12, 0);
-
-        // // Populate the array with actual income data
-        // foreach ($monthlyIncome as $income) {
-        //     $monthsOfYear[$income->month] = $income->total_income;
-        // }
-
-        // // Populate the array with actual invoices data
-        // foreach ($monthlyInvoices as $invoice) {
-        //     $monthsOfYearInvoices[$invoice->month] = $invoice->total_invoices;
-        // }
 
         return view('backend.dashboard.main-dashboard', compact('totalRevenue', 'totalInvoices', 'availableTablesCount', 'inUseTablesCount', 'monthsOfYear', 'monthsOfYearInvoices', 'selectedYear', 'selectedInvoiceYear'));
     }
@@ -83,11 +52,21 @@ class DashboardController extends Controller
 
     private function getMonthlyInvoicesData($year)
     {
-        // Fetch monthly total invoices for the selected year
+        // // Fetch monthly total invoices for the selected year
+        // $monthlyInvoices = Order::select(
+        //     DB::raw('MONTH(created_at) as month'),
+        //     DB::raw('COUNT(id) as total_invoices')
+        // )->whereYear('created_at', $year)
+        //     ->groupBy('month')
+        //     ->orderBy('month')
+        //     ->get();
+
+        // Fetch monthly total invoices for the selected year with status = 1
         $monthlyInvoices = Order::select(
             DB::raw('MONTH(created_at) as month'),
             DB::raw('COUNT(id) as total_invoices')
         )->whereYear('created_at', $year)
+            ->where('status', 1)
             ->groupBy('month')
             ->orderBy('month')
             ->get();
