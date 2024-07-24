5f3ff81be8a3ee4dc682bdfb5a6e5cda51eb706f
Thura Sitt Naing <coding2am@gmail.com>
2024-07-24 10:36:25 +0630

    Fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,7 +63,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin' =>  \App\Http\Middleware\CheckAdmin::class,
-        'waite' =>  \App\Http\Middleware\CheckWaiter::class,
+        'waiter' =>  \App\Http\Middleware\CheckWaiter::class,
         'kitchen' =>  \App\Http\Middleware\CheckKitchen::class,
     ];
 }
